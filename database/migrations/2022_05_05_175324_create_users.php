ef094395d29a1c63c6feb8682341059fc0bd4a6f
Jaime López <61801163+jaimelg27@users.noreply.github.com>
2022-05-05 20:02:45 +0200

    Initial Commit

--- database/migrations/2022_05_05_175324_create_users.php
+++ database/migrations/2022_05_05_175324_create_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -14,13 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->string('dni', 8)->primary();
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->date('birth_date');
+            $table->integer('telephone');
         });
     }
 
