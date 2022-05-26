73ddc09f240d5ccd543af1cb5e9382615a3ade20
Jaime López <61801163+jaimelg27@users.noreply.github.com>
2022-05-26 16:52:02 +0200

    Migration users modified.

--- database/migrations/2022_05_05_175324_create_users.php
+++ database/migrations/2022_05_05_175324_create_users.php
@@ -18,9 +18,10 @@ class CreateUsers extends Migration
             $table->string('name');
             $table->string('surname');
             $table->string('email')->unique();
-            $table->string('password');
             $table->date('birth_date');
             $table->string('telephone', 9);
+            $table->string('password');
+            $table->timestamp('crated_at');
         });
     }
 
