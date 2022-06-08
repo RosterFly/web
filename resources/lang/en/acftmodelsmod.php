d219ceb636bfdfca000d6e3d3e2276a19e973c73
Jaime López <61801163+jaimelg27@users.noreply.github.com>
2022-06-08 19:04:20 +0200

    Lang aircraft models

---
+++ resources/lang/en/acftmodelsmod.php
@@ -0,0 +1,29 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'newAcftModel'=>'New Aircraft Model',
+    'editAcftModel'=>'Edit Aircraft Model',
+    'acftmodel'=>'Aircraft Model',
+    'acftmanufacturer'=>'Aircraft Manufacturer',
+    'acfttype'=>'Aircraft Type',
+    'ICAOCode'=>'ICAO Code',
+    'IATACode'=>'IATA Code',
+    'type'=>'Type',
+    'wingtype'=>'Wing Type',
+    'rotatorywing'=>'Rotatory Wing',
+    'fixedwing'=>'Fixed Wing',
+    'airplane'=>'Airplane',
+    'helicopter'=>'Helicopter',
+    'save'=>'Save',
+    'create'=>'Create Aircraft Model',
+];
