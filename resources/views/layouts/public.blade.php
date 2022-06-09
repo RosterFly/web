45a27ffe7743c157be7c4bbb4f72dfad72b168dd
Jaime López <61801163+jaimelg27@users.noreply.github.com>
2022-06-09 09:12:13 +0200

    Public index layout modified

---
+++ resources/views/layouts/public.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>@lang('pages-name.index')</title>
+    <link rel="shortcut icon" href="{{asset('favicon.png')}}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{asset('favicon.png')}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicon.png')}}">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Mobland - Mobile App Landing Page Template">
+    <meta name="keywords" content="HTML5, bootstrap, mobile, app, landing, ios, android, responsive">
+
+    <link rel="dns-prefetch" href="//fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500" rel="stylesheet">
+
+    @yield('css_before')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css">
+    <link href="{{asset('resources/public/style.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.1/css/flag-icon.min.css">
+    @yield('css_after')
+
+</head>
+<body data-spy="scroll" data-target="#navbar" data-offset="30">
+
+@yield('content')
+
+@yield('js_before')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+<script src="{{asset('resources/public/script.js')}}"></script>
+@yield('js_after')
+</body>
+
+</html>
+
+
+
--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -1,30 +1,6 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.public')
 
-<head>
-    <title>@lang('pages-name.index')</title>
-    <link rel="shortcut icon" href="{{asset('favicon.png')}}">
-    <link rel="icon" type="image/png" sizes="192x192" href="{{asset('favicon.png')}}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicon.png')}}">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Mobland - Mobile App Landing Page Template">
-    <meta name="keywords" content="HTML5, bootstrap, mobile, app, landing, ios, android, responsive">
-
-    <link rel="dns-prefetch" href="//fonts.googleapis.com">
-    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500" rel="stylesheet">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css">
-    <link href="{{asset('resources/public/style.css')}}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.1/css/flag-icon.min.css">
-
-</head>
-
-<body data-spy="scroll" data-target="#navbar" data-offset="30">
-
-
+@section('content')
 <div class="nav-menu fixed-top">
     <div class="container">
         <div class="row">
@@ -381,17 +357,18 @@
 <div class="section bg-gradient">
     <div class="container">
         <div class="call-to-action">
-    </div>
-    <h2>@lang('index.download')</h2>
-    <p class="tagline">@lang('index.downloadText')</p>
-    <div class="my-4">
+        </div>
+        <h2>@lang('index.download')</h2>
+        <p class="tagline">@lang('index.downloadText')</p>
+        <div class="my-4">
 
-        <a href="#" class="btn btn-light"><img src="{{asset('resources/images/appleicon.png')}}" alt="icon"> App
-            Store</a>
-        <a href="#" class="btn btn-light"><img src="{{asset('resources/images/playicon.png')}}" alt="icon"> Google play</a>
+            <a href="#" class="btn btn-light"><img src="{{asset('resources/images/appleicon.png')}}" alt="icon"> App
+                Store</a>
+            <a href="#" class="btn btn-light"><img src="{{asset('resources/images/playicon.png')}}" alt="icon"> Google
+                play</a>
+        </div>
+        <p class="text-primary"><small><i>@lang('index.downloadVersion')</i></small></p>
     </div>
-    <p class="text-primary"><small><i>@lang('index.downloadVersion')</i></small></p>
-</div>
 </div>
 
 </div>
@@ -416,12 +393,4 @@
 <footer class="my-5 text-center">
     <p class="mb-2"><small>@lang('index.copyright')</a></small></p>
 </footer>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-<script src="{{asset('resources/public/script.js')}}"></script>
-
-</body>
-
-</html>
+@endsection
